d0d579df1be335acf7cd151829bd7a4f47cbcf55
DarrenBellew <islarf6546@gmail.com>
2016-12-17 23:26:10 +0000

    AndroidStudio setup for prototype

---
+++ lock_screen_prototype/app/src/main/java/com/islarf6546/gmail/lock_screen_prototype/MainActivity.java
@@ -0,0 +1,13 @@
+package com.islarf6546.gmail.lock_screen_prototype;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
