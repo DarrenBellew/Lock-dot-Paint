6556e5870a89da983d040c2d0c52dc207d721aeb
Darren Bellew <islarf6546@gmail.com>
2017-02-08 23:13:24 +0000

    Created Geometry Math, Possible Angle Formula
    Created a class to perform geometry math, and some formula to possibly calculate angles between two lines.   The stroke class is going to need to be reviewd and probably recreated on how the DrawView stores the coordinates into lines.

---
+++ Lock_prototype_1/app/src/main/java/com/islarf6546/gmail/lock_prototype_1/GeomeryMath.java
@@ -0,0 +1,8 @@
+package com.islarf6546.gmail.lock_prototype_1;
+
+/**
+ * Created by islarf on 08/02/17.
+ */
+
+public class GeomeryMath {
+}
---
+++ Lock_prototype_1/app/src/main/java/com/islarf6546/gmail/lock_prototype_1/Line.java
@@ -0,0 +1,8 @@
+package com.islarf6546.gmail.lock_prototype_1;
+
+/**
+ * Created by islarf on 06/02/17.
+ */
+
+public class Line {
+}
